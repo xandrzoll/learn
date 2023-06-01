7382e8ca5d79a2e74ae13fcd00692e1b36d00db1
zarocool <xandr.rudkovsky@gmail.com>
2023-06-01 21:36:33 +0300

    edit task 3

--- netology/data_engineering/homework_2_spark_sql.py
+++ netology/data_engineering/homework_2_spark_sql.py
@@ -72,22 +72,22 @@ window = Window.partitionBy("location").orderBy(col("new_cases").desc())
 window = Window.partitionBy("location").orderBy(col("date"))
 (
     data
+    .withColumn('cases_prev_day', lag('new_cases', 1).over(window))
     .filter((col('date') >= '2021-03-22') & (col('date') <= '2021-03-28') & (col('iso_code') == 'RUS'))
-    .withColumn('cases_prev_day', lag('new_cases', -1).over(window))
     .select('iso_code', 'date', 'location', 'new_cases', 'cases_prev_day', (col('new_cases') - col('cases_prev_day')).alias('cases_delta'))
-    .fillna(0)
+    .sort(col('date').desc())
     .show(20)
 )
 # +--------+----------+--------+---------+--------------+-----------+
 # |iso_code|      date|location|new_cases|cases_prev_day|cases_delta|
 # +--------+----------+--------+---------+--------------+-----------+
-# |     RUS|2021-03-22|  Russia|   9195.0|        8369.0|      826.0|
-# |     RUS|2021-03-23|  Russia|   8369.0|        8769.0|     -400.0|
-# |     RUS|2021-03-24|  Russia|   8769.0|        9128.0|     -359.0|
-# |     RUS|2021-03-25|  Russia|   9128.0|        9073.0|       55.0|
-# |     RUS|2021-03-26|  Russia|   9073.0|        8783.0|      290.0|
-# |     RUS|2021-03-27|  Russia|   8783.0|        8979.0|     -196.0|
-# |     RUS|2021-03-28|  Russia|   8979.0|           0.0|        0.0|
+# |     RUS|2021-03-28|  Russia|   8979.0|        8783.0|      196.0|
+# |     RUS|2021-03-27|  Russia|   8783.0|        9073.0|     -290.0|
+# |     RUS|2021-03-26|  Russia|   9073.0|        9128.0|      -55.0|
+# |     RUS|2021-03-25|  Russia|   9128.0|        8769.0|      359.0|
+# |     RUS|2021-03-24|  Russia|   8769.0|        8369.0|      400.0|
+# |     RUS|2021-03-23|  Russia|   8369.0|        9195.0|     -826.0|
+# |     RUS|2021-03-22|  Russia|   9195.0|        9215.0|      -20.0|
 # +--------+----------+--------+---------+--------------+-----------+
 
 spark.stop()
